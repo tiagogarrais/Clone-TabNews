de6db8280301e098641af15343aaabdc9bf4b358
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-11-11 05:48:23 +0000

    save

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET to api/v1/status should retorn 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 });
